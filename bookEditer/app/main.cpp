dd968a130ccb331492939937761bd4b0aade370d
xinyong_l <xinyong_l@qu-in.com>
2025-04-16 12:04:50 +0800

    feat: 调整结构，增加qputenv

--- bookEditer/app/main.cpp
+++ bookEditer/app/main.cpp
@@ -1,9 +1,11 @@
-#include <QApplication>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QSettings>
 
 #include <ToolUnit/OpenBookLog/alogger.h>
+
+
+#include <QtQuick/QQuickWindow>
 #include <Config/config.h>
 
 int main(int argc, char *argv[])
@@ -11,10 +13,24 @@ int main(int argc, char *argv[])
     PairIdea::ALogger logger;
     logger.installLogHandler(PairIdea::AppName);
     qInfo() << ".................Application Start .................";
+
+    qputenv("QT_WIN_DEBUG_CONSOLE", "attach");
+    qputenv("QSG_INFO", "1");
+    qputenv("QT_QUICK_CONTROLS_STYLE", "Default");
+    // qputenv("QT_QUICK_CONTROLS_STYLE", "Basic");
+    // #if 0
+    qputenv("QSG_RHI_BACKEND", "opengl");
+    //qputenv("QSG_RHI_HDR", "scrgb");
+    //qputenv("QT_QPA_DISABLE_REDIRECTION_SURFACE", "1");
+    QGuiApplication::setHighDpiScaleFactorRoundingPolicy(
+        Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
+    // #endif
+    QQuickWindow::setDefaultAlphaBuffer(true);
+
     // Setting Format is ini
     QSettings::setDefaultFormat(QSettings::IniFormat);
 
-    QApplication app(argc, argv);
+    QGuiApplication app(argc, argv);
     app.setApplicationName(PairIdea::AppName);
     app.setOrganizationName(PairIdea::CompanyName);
     app.setApplicationVersion(PairIdea::AppVersion);
@@ -26,10 +42,10 @@ int main(int argc, char *argv[])
         &app,
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
+    // QWK::registerTypes(&engine);
     engine.loadFromModule("Main", "Main");
     // const QUrl url(QStringLiteral("../view/Main.qml"));
     // engine.load(url);
-
 
     qInfo() << ".................Application End .................";
     logger.uninstallLogHandler();
